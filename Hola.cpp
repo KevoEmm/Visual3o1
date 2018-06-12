ce8bd47b450afac762c7a2aeeeed3fe130790acd
AraBriones <Ara_biones_9@hotmail.com>
2018-06-12 09:07:13 -0500

    Arael J Briones Silva

--- Hola.cpp
+++ Hola.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 //hola by jose luis garcia
 //hola by Alejandro Bautista OwO
+//hola by Arael J Briones Solva:v <3 
 main()
 {
-	cout<<"Hola mundo"<<endl;
+	cout<<"Neto es un mugroso "<<endl;
 }
